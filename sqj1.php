f3df4b3342281fb7ec728c223e51f7f7cc48e0a7
A GUEI <75734164+Ericostco@users.noreply.github.com>
2024-05-06 18:05:29 +0800

    Update sqj1.php

--- sqj1.php
+++ sqj1.php
@@ -1,6 +1,5 @@
 <?php
-if (isset($_POST["login"]))
-{
+if (isset($_POST["login"])) {
         $link = mysqli_connect('localhost', 'secpro', 'Snsysu001');
 	mysqli_set_charset('utf8', $link);
 	$db_selected = mysqli_select_db($link, 'Secure_Programming_Test');
@@ -10,23 +9,18 @@ if (isset($_POST["login"]))
 		"' AND password='" . $_POST["password"] . "'";
 
 	$result = mysqli_query($link, $query) 
-		or die("MySQL Query Error : " . mysqli_error() . "   SQL: " . $q
-uery);
+		or die("MySQL Query Error : " . mysqli_error() . "   SQL: " . $query);
 	
 	$match_count = mysqli_num_rows($result);
 
-	if ($match_count) 
-	{
+	if ($match_count) {
 		mysqli_free_result($result);
 		
 		mysqli_close($link);
-		header("Location: http://120.113.173.21/sqj1_s.php?user=" . $_PO
-ST["username"]);
+		header("Location: http://120.113.173.21/sqj1_s.php?user=" . $_POST["username"]);
 	}
-	else
-	{
-		header("Location: http://" . $_SERVER["HTTP_HOST"] . $_SERVER["S
-CRIPT_NAME"]); 
+	else {
+		header("Location: http://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"]); 
 	}
 }
 ?>
@@ -35,11 +29,12 @@ CRIPT_NAME"]);
 <title>SQL Injection Test 1</title>
 </head>
 <body>
-<div style="text-align: center;"><span>
-<big style="font-weight: bold; color: red;"><big><big>SQL Injection 練習</big></
-big></big><br>
-<br>
-請輸入登入的帳號與密碼</span><br>
+<div style="text-align: center;">
+	<span>
+		<big style="font-weight: bold; color: red;"><big><big>SQL Injection 練習</big></big></big><br><br>
+		請輸入登入的帳號與密碼
+	</span>
+	<br>
 </div>
 <br>
 <form method="post" name="form1" id="form1">
